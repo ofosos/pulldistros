c0d5d991948a365d356812fef5d00a7c43f23dd1
Mark Meyer <mark@ofosos.org>
2018-01-21 19:21:54 +0100

    Fix bugs and change to html5lib parser to add summaries

--- pulldistros.py
+++ pulldistros.py
@@ -1,12 +1,10 @@
 from bs4 import BeautifulSoup
-from bs4.element import Tag
 import requests
-import json
 import requests_cache
 
 
 requests_cache.install_cache('/tmp/req.cache', backend='sqlite',
-                             expire_after=1500)
+                             expire_after=3600)
 
 
 def distros():
@@ -28,9 +26,18 @@ def distinfo(distro):
     r = requests.get('http://distrowatch.com/table.php?distribution={0}'
                      .format(distro))
 
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html5lib')
 
     dist = {}
+
+    summary = [td for td in soup.find_all('td') if td.get('class') ==
+               ["TablesTitle"]][0]
+    for li in summary.find_all("li"):
+        tpl = li.text.split(':')
+        if len(tpl) > 1:
+            text = tpl[0].strip()
+            value = tpl[1].strip()
+            dist[text] = value
 
     infotags = soup.find('th', string='Alternative User Forums').parent.parent
     for row in infotags.select('tr[class=Background]'):
@@ -44,18 +51,28 @@ def distinfo(distro):
     releases = {}
 
     relnames = []
-    reltags = soup.select('td[class=TablesInvert]')
-    for reltag in reltags:
+    reltag = soup.find('td', attrs={'class': 'TablesInvert'})
+
+    releases[reltag.text] = {}
+    relnames.append(reltag.text)
+
+    while reltag.find_next_sibling('td'):
+        reltag = reltag.find_next_sibling('td')
+
         releases[reltag.text] = {}
         relnames.append(reltag.text)
 
-    infotbl = soup.find('th', string='Full Package List').parent.parent
-    for row in infotbl.find_all('tr'):
-        thel = row.find('th')
-        header = thel.text
-        vals = row.find_all('td')
-        for i in range(len(vals)):
-            releases[relnames[i]][header] = vals[i].text
+    try:
+        infotbl = soup.find('th', string='Full Package List').parent.parent
+        for r in infotbl.find_all('tr'):
+            thel = r.find('th')
+            header = thel.text
+            vals = r.find_all('td')
+
+            for i in range(len(vals)):
+                releases[relnames[i]][header] = vals[i].text
+    except:
+        pass
 
     dist['releases'] = releases
     return dist
